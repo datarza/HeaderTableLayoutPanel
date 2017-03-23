c93791c04b3213ae075ac717ad0a05d482aef1ff
CanadianBeaver <ontariobeaver@gmail.com>
2017-03-22 20:45:30 -0400

    Changed names and Category

--- src/HeaderTableLayoutPanel.cs
+++ src/HeaderTableLayoutPanel.cs
@@ -12,44 +12,53 @@ namespace CBComponents
 {
   public class HeaderTableLayoutPanel : System.Windows.Forms.TableLayoutPanel
   {
-    [Browsable(true), DefaultValue(null)]
-    public string Caption
+    /// <summary>
+    /// Header text
+    /// </summary>
+    [Browsable(true), DefaultValue(null), Category("Header")]
+    public string CaptionText
     {
-      get { return this.caption; }
+      get { return this.captionText; }
       set
       {
-        if (this.caption != value)
+        if (this.captionText != value)
         {
-          this.caption = value;
+          this.captionText = value;
           this.CalculateCaptionParams();
           Invalidate();
         }
       }
     }
-    private string caption = null;
+    private string captionText = null;
 
+    /// <summary>
+    /// Drawing styles for Header
+    /// </summary>
     public enum HighlightCaptionStyle
     {
       ForeColor, HighlightColor, HighlightStyle, NavisionAxaptaStyle
     }
 
-    [Browsable(true), DefaultValue(HighlightCaptionStyle.ForeColor)]
+    /// <summary>
+    /// Drawing style of Header
+    /// </summary>
+    [Browsable(true), DefaultValue(HighlightCaptionStyle.ForeColor), Category("Header")]
     public HighlightCaptionStyle CaptionStyle
     {
-      get { return this.captionMode; }
+      get { return this.captionStyle; }
       set
       {
-        if (this.captionMode != value)
+        if (this.captionStyle != value)
         {
-          this.captionMode = value;
+          this.captionStyle = value;
           this.CalculateCaptionParams();
           Invalidate();
         }
       }
     }
-    private HighlightCaptionStyle captionMode = HighlightCaptionStyle.ForeColor;
+    private HighlightCaptionStyle captionStyle = HighlightCaptionStyle.ForeColor;
 
-    [Browsable(true), DefaultValue((byte)2)]
+    [Browsable(true), DefaultValue((byte)2), Category("Header")]
     public byte CaptionLineWidth
     {
       get { return this.captionLineWidth; }
@@ -90,14 +99,14 @@ namespace CBComponents
     private int captionTextWidth;
     private int captionTextHeight;
     private Color captionTextColor;
-    private Color captionGBColor;
-    private Color captionGEColor;
+    private Color captionLineBeginColor;
+    private Color captionLineEndColor;
     private void CalculateCaptionParams()
     {
-      if (!string.IsNullOrEmpty(this.Caption))
+      if (!string.IsNullOrEmpty(this.captionText))
         using (var g = this.CreateGraphics())
         {
-          var _size = g.MeasureString(this.Caption, this.Font).ToSize();
+          var _size = g.MeasureString(this.captionText + "*", this.Font).ToSize();
           this.captionTextWidth = _size.Width;
           this.captionTextHeight = _size.Height;
         }
@@ -106,17 +115,17 @@ namespace CBComponents
         this.captionTextWidth = 0;
         this.captionTextHeight = 0;
       }
-      if (this.captionMode == HighlightCaptionStyle.ForeColor)
+      if (this.captionStyle == HighlightCaptionStyle.ForeColor)
       {
         this.captionTextColor = this.ForeColor;
-        this.captionGBColor = this.ForeColor;
-        this.captionGEColor = this.BackColor;
+        this.captionLineBeginColor = this.ForeColor;
+        this.captionLineEndColor = this.BackColor;
       }
       else
       {
-        this.captionTextColor = this.captionMode == HighlightCaptionStyle.HighlightStyle ? SystemColors.HighlightText : SystemColors.Highlight;
-        this.captionGBColor = SystemColors.MenuHighlight;
-        this.captionGEColor = this.BackColor; 
+        this.captionTextColor = this.captionStyle == HighlightCaptionStyle.HighlightStyle ? SystemColors.HighlightText : SystemColors.Highlight;
+        this.captionLineBeginColor = SystemColors.MenuHighlight;
+        this.captionLineEndColor = this.BackColor; 
       }
     }
 
@@ -127,8 +136,8 @@ namespace CBComponents
         var result = base.DisplayRectangle;
         int resize = 0;
         if (this.captionTextHeight > 0) resize = this.captionTextHeight + 2;
-        if (this.captionMode == HighlightCaptionStyle.HighlightStyle) resize += this.captionLineWidth * 2;
-        else if (this.captionMode != HighlightCaptionStyle.NavisionAxaptaStyle) resize += this.captionLineWidth;
+        if (this.captionStyle == HighlightCaptionStyle.HighlightStyle) resize += this.captionLineWidth * 2;
+        else if (this.captionStyle != HighlightCaptionStyle.NavisionAxaptaStyle) resize += this.captionLineWidth;
         result.Height -= resize;
         result.Offset(0, resize);
         return result;
@@ -140,8 +149,8 @@ namespace CBComponents
       var result = base.SizeFromClientSize(clientSize);
       int resize = 0;
       if (this.captionTextHeight > 0) resize = this.captionTextHeight + 2;
-      if (this.captionMode == HighlightCaptionStyle.HighlightStyle) resize += this.captionLineWidth * 2;
-      else if (this.captionMode != HighlightCaptionStyle.NavisionAxaptaStyle) resize += this.captionLineWidth;
+      if (this.captionStyle == HighlightCaptionStyle.HighlightStyle) resize += this.captionLineWidth * 2;
+      else if (this.captionStyle != HighlightCaptionStyle.NavisionAxaptaStyle) resize += this.captionLineWidth;
       result.Height += resize;
       return result;
     }
@@ -150,29 +159,31 @@ namespace CBComponents
     {
       base.OnPaint(e);
       // draw gradient
-      if (this.captionMode == HighlightCaptionStyle.HighlightStyle)
+      if (this.captionStyle == HighlightCaptionStyle.HighlightStyle)
       { // HighlightCaptionStyle.HighlightStyle allways draw
-        using (Brush _gradientBrush = new LinearGradientBrush(new Point(0, 0), new Point(this.Width, 0), this.captionGBColor, this.captionGEColor))
+        using (Brush _gradientBrush = new LinearGradientBrush(new Point(0, 0), new Point(this.Width, 0), this.captionLineBeginColor, this.captionLineEndColor))
         using (Pen _gradientPen = new Pen(_gradientBrush, this.captionLineWidth * 2 + this.captionTextHeight))
           e.Graphics.DrawLine(_gradientPen, 0, _gradientPen.Width / 2, this.Width, _gradientPen.Width / 2);
       }
       else if (this.captionLineWidth > 0)
-        if (this.captionMode != HighlightCaptionStyle.NavisionAxaptaStyle)
+        if (this.captionStyle != HighlightCaptionStyle.NavisionAxaptaStyle)
         { // HighlightCaptionMode.ForeColor | HighlightCaptionMode.SystemColorsHighlight
-          using (Brush _gradientBrush = new LinearGradientBrush(new Point(0, 0), new Point(this.Width, 0), this.captionGBColor, this.captionGEColor))
+          using (Brush _gradientBrush = new LinearGradientBrush(new Point(0, 0), new Point(this.Width, 0), this.captionLineBeginColor, this.captionLineEndColor))
           using (Pen _gradientPen = new Pen(_gradientBrush, this.captionLineWidth))
             e.Graphics.DrawLine(_gradientPen, 0, this.captionTextHeight + this.captionLineWidth / 2, this.Width, this.captionTextHeight + this.captionLineWidth / 2);
         }
         else if (this.captionTextWidth + 1 < this.Width)
         { // HighlightCaptionMode.NavisionAxapta
-          using (Brush _gradientBrush = new LinearGradientBrush(new Point(this.captionTextWidth, 0), new Point(this.Width, 0), this.captionGBColor, this.captionGEColor))
+          using (Brush _gradientBrush = new LinearGradientBrush(new Point(this.captionTextWidth, 0), new Point(this.Width, 0), this.captionLineBeginColor, this.captionLineEndColor))
           using (Pen _gradientPen = new Pen(_gradientBrush, this.captionLineWidth > this.captionTextHeight ? this.captionTextHeight : this.captionLineWidth))
             e.Graphics.DrawLine(_gradientPen, this.captionTextWidth, this.captionTextHeight / 2 + 1, this.Width, this.captionTextHeight / 2 + 1);
+          //using (Pen _roundPen = new Pen(this.captionGBColor, (this.captionLineWidth > this.captionTextHeight ? this.captionTextHeight : this.captionLineWidth) - 2))
+            //e.Graphics.DrawLine(_roundPen, this.captionTextWidth - 1, this.captionTextHeight / 2 + 1, this.captionTextWidth, this.captionTextHeight / 2 + 1);
         }
       // draw Text
       if (this.captionTextHeight > 0)
         using (Brush _textBrush = new SolidBrush(this.captionTextColor))
-          e.Graphics.DrawString(this.Caption, this.Font, _textBrush, 0, this.captionMode == HighlightCaptionStyle.HighlightStyle ? this.CaptionLineWidth : 0);
+          e.Graphics.DrawString(this.captionText, this.Font, _textBrush, 0, this.captionStyle == HighlightCaptionStyle.HighlightStyle ? this.CaptionLineWidth : 0);
     }
 
   }
