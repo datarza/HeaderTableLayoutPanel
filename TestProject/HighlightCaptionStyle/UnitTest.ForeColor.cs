1ab2eb00bf914916180292b5fa3ac8a52b474934
CanadianBeaver <ontariobeaver@gmail.com>
2017-03-25 14:27:10 -0400

    TestProject has been optimized

--- TestProject/HighlightCaptionStyle/UnitTest.ForeColor.cs
+++ TestProject/HighlightCaptionStyle/UnitTest.ForeColor.cs
@@ -8,16 +8,10 @@ namespace TestProject
     [TestMethod]
     public void ForeColor()
     {
-      // Creating panel
-      System.Windows.Forms.Form form;
-      CBComponents.HeaderTableLayoutPanel panel;
-      this.CreateTestingControl(out form, out panel);
-
-      // running tests
-      // TODO: write tests
-
-      // Disposing panel
-      this.DisposeTestingControl(ref form, ref panel);
+      using (var _to = new TestObjects())
+      {
+        // TODO: write tests
+      }
     }
   }
 }
---
+++ TestProject/TestObjectsClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestProject
+{
+  /// <summary>
+  /// Form and Panel for testing
+  /// </summary>
+  internal class TestObjects : IDisposable
+  {
+    public System.Windows.Forms.Form Form { get; private set; }
+    public CBComponents.HeaderTableLayoutPanel Panel { get; private set; }
+
+    public TestObjects()
+    {
+      this.Form = new System.Windows.Forms.Form();
+      this.Form.SuspendLayout();
+      this.Form.SetDesktopBounds(0, 0, 640, 480);
+
+      this.Panel = new CBComponents.HeaderTableLayoutPanel();
+      this.Form.Controls.Add(this.Panel);
+      this.Panel.SetBounds(0, 0, 320, 240);
+
+      this.Form.ResumeLayout(false);
+      this.Form.PerformLayout();
+    }
+
+    private bool disposedValue = false; 
+    public void Dispose()
+    {
+      if (!disposedValue)
+      {
+        this.Form.Dispose();
+        this.Form = null;
+        this.Panel = null;
+        disposedValue = true;
+      }
+    }
+
+  }
+}
