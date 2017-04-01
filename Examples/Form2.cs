47be38cb200ec466c4622b8d4f3858709146feb6
CanadianBeaver <ontariobeaver@gmail.com>
2017-04-01 15:52:05 -0400

    Form2

--- Examples/Form2.cs
+++ Examples/Form2.cs
@@ -14,6 +14,10 @@ namespace Examples
     public Form2()
     {
       InitializeComponent();
+      this.Shown += delegate { this.textBox1.Focus(); };
+      this.FormClosed += delegate { Application.Exit(); };
+
+      // buttons for changing CaptionStyle
       Type _type = typeof(HeaderTableLayoutPanel.HighlightCaptionStyle);
       foreach (var _style in Enum.GetValues(_type))
         this.toolStrip1.Items.Add(new ToolStripButton() {
@@ -21,13 +25,13 @@ namespace Examples
           Tag = _style,
           DisplayStyle = ToolStripItemDisplayStyle.Text });
       this.toolStrip1.Items[1].PerformClick();
-      this.Shown += delegate { this.textBox1.Focus(); };
-      this.FormClosed += delegate { Application.Exit(); };
 
+      // strip for changing width of CaptionLineWidth
       this.toolStripComboBox1.Items.AddRange(new object[] { (byte)0, (byte)1, (byte)2, (byte)3, (byte)4, (byte)6, (byte)8, (byte)10, (byte)20 });
       this.toolStripComboBox1.SelectedIndexChanged += toolStripComboBox1_SelectedIndexChanged;
       this.toolStripComboBox1.SelectedIndex = 2;
-
+      
+      // strip for changing text of CaptionText
       this.toolStripTextBox1.TextChanged += toolStripTextBox1_TextChanged;
 
     }
